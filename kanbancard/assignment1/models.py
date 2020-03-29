6c5e46e38f46a1eca0e6f5d861028104a06617df
Nishant Shah <nishantshah@apple.com>
2020-03-29 00:39:34 -0700

    create a project, and added an app with basic model

---
+++ kanbancard/assignment1/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Cards(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200,blank=True)
+    STATUS_CHOICES = [('to-do','to-do'),('in-process','in-process'),('done','done'),]
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
