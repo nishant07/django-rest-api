240deaeed5025556dea87d4d7cacead99ee863fe
Nishant Shah <nishantshah@apple.com>
2020-03-30 19:31:35 -0700

    assignment3 complete, only the owner can see relevant objects

--- kanbancard/assignment3/views.py
+++ kanbancard/assignment3/views.py
@@ -7,9 +7,14 @@ from django.core import exceptions
 from assignment3.custom_permissions import IsOwner, IsCardOwner
 
 class CardCollection(generics.ListCreateAPIView):
-    queryset = Card.objects.all()
+    # queryset = Card.objects.all()
     serializer_class = CardSerializer
+
     permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Card.objects.filter(owner=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -19,9 +24,12 @@ class CardItem(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
 class TaskCollection(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+    # queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsCardOwner]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
         if serializer.validated_data['card'].owner != self.request.user:
