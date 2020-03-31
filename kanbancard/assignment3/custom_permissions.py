5817fe5652c32b510a50fca450e5f6c9d122c032
Nishant Shah <nishantshah@apple.com>
2020-03-30 18:36:44 -0700

    assignment3 almost complete, GET collection shows all objects, but items are restricted

---
+++ kanbancard/assignment3/custom_permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsCardOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.card.owner == request.user
--- kanbancard/assignment3/views.py
+++ kanbancard/assignment3/views.py
@@ -2,19 +2,34 @@ from django.shortcuts import render
 from assignment3.models import Card, Task
 from assignment3.serializers import CardSerializer, TaskSerializer
 from rest_framework import generics
+from rest_framework import generics, permissions
+from django.core import exceptions
+from assignment3.custom_permissions import IsOwner, IsCardOwner
 
 class CardCollection(generics.ListCreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class CardItem(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
 class TaskCollection(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsCardOwner]
+
+    def perform_create(self, serializer):
+        if serializer.validated_data['card'].owner != self.request.user:
+            raise exceptions.PermissionDenied
+        else:
+            serializer.save()
 
 class TaskItem(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsCardOwner]
